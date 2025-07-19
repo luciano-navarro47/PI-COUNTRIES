706ea775068bef06643993e664ae3b712cfb7ec3
Coderman47 <askinglucho@gmail.com>
2025-07-19 15:54:10 -0300

    accepting changes in api and client

--- api/index.js
+++ api/index.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-const { createCountriesToDb } = require("./src/routes/Controllers/controllers.js")
-const {PORT } = process.env
-
-
-// Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
-  createCountriesToDb()
-  server.listen( PORT, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
-  });
-});
-=======
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const {
@@ -52,4 +18,3 @@ const PORT = process.env.PORT || 3001;
     );
   }
 })();
->>>>>>> f72f63d (update react-scripts to 5.0.1 | refactor createCountriesToDb)
--- api/src/routes/Controllers/controllers.js
+++ api/src/routes/Controllers/controllers.js
@@ -2,39 +2,6 @@ const axios = require("axios");
 const { Country, Activities } = require("../../db.js");
 
 const createCountriesToDb = async () => {
-<<<<<<< HEAD
-  const apiInfo = await axios.get("https://restcountries.com/v3/all");
-  // console.log(apiInfo.data)
-  const data = await apiInfo.data.map((el) => {
-    return {
-      id: el.cca3,
-      name: el.name.common,
-      continent: el.region,
-      capital: el.capital ? el.capital[0] : "x",
-      subregion: el.subregion ? el.subregion : "x",
-      area: el.area,
-      population: el.population,
-      flags: el.flags[1],
-    };
-  });
-
-  data.forEach((country) => {
-    Country.findOrCreate({
-      where: {
-        id: country.id,
-        name: country.name,
-        continent: country.continent,
-        capital: country.capital,
-        subregion: country.subregion,
-        area: country.area,
-        population: country.population,
-        flags: country.flags,
-      },
-      default:{
-
-      }
-    });
-=======
 
   const count = await Country.count();
   if (count > 0) {
@@ -63,16 +30,11 @@ const createCountriesToDb = async () => {
 
   await Country.bulkCreate(countryList, {
     ignoreDuplicates: true
->>>>>>> f72f63d (update react-scripts to 5.0.1 | refactor createCountriesToDb)
   });
 };
 
 const getAllCountriesDb = async () => {
   const allCountries = await Country.findAll({
-<<<<<<< HEAD
-   
-=======
->>>>>>> f72f63d (update react-scripts to 5.0.1 | refactor createCountriesToDb)
     include: [
       {
         model: Activities,
@@ -80,10 +42,6 @@ const getAllCountriesDb = async () => {
       },
     ],
   });
-<<<<<<< HEAD
-  // console.log("hellouuuuu",allCountries)
-=======
->>>>>>> f72f63d (update react-scripts to 5.0.1 | refactor createCountriesToDb)
   return allCountries;
 };
 
@@ -101,22 +59,13 @@ const getAllActivities = async () => {
       },
     ],
   });
-<<<<<<< HEAD
-  // console.log("getAllActivities", data)
-=======
->>>>>>> f72f63d (update react-scripts to 5.0.1 | refactor createCountriesToDb)
   return data;
 };
 
 const findCountryById = (id, countries) => {
   const filterCountry = countries.find(
-<<<<<<< HEAD
-    (country) => country.id.toLowerCase() === id.toLowerCase());
-  // console.log("soy el find()" , filterCountry)
-=======
     (country) => country.id.toLowerCase() === id.toLowerCase()
   );
->>>>>>> f72f63d (update react-scripts to 5.0.1 | refactor createCountriesToDb)
   if (!filterCountry) {
     throw new Error("A country with that ID was not found");
   }
@@ -135,11 +84,7 @@ const createActivity = async (name, difficulty, duration, season, paises) => {
       name: paises,
     },
   });
-<<<<<<< HEAD
-  console.log(countries)
-=======
   console.log(countries);
->>>>>>> f72f63d (update react-scripts to 5.0.1 | refactor createCountriesToDb)
   newActivity.addCountry(countries);
 };
 
